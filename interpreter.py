ef49258491ac807beea31344584e295f09d63860
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-01-14 21:07:51 -0600

    Basic interpretaion
    can skip comments and execute files now

--- interpreter.py
+++ interpreter.py
@@ -1,2 +1,20 @@
+from datetime import date
+from logger import *
+import os
+
+today = date.today()
+date = today.strftime("%m/%d/%y")
+
 def interpret(letter):
-    print(f'Reading drive letter: {letter}')
+    file = open(letter + ":\\" + "Autorun.inf", "r")
+    for line in file:
+        if line[0] == "[":
+            pass
+        if line[0] == ";":
+            pass
+        if "run" in line:
+            executeLocation1 = line.split("= ")
+            executeLocation2 = letter + ":\\" + executeLocation1[1]
+            executeLocation3 = executeLocation2.replace("\n","");
+            os.startfile(executeLocation3)
+            logadd("[#]", f'[{date}]', f'launched {executeLocation3} from drive {letter}')
