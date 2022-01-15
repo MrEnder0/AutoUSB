2d97835f632dcbd023d21e3214dac8fd035ff762
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-01-14 19:59:49 -0600

    Replaced print logs with log function
    will make into log file soon

--- filecheck.py
+++ filecheck.py
@@ -1,15 +1,17 @@
+from datetime import date
 from logger import *
 import os
 
 letters = ["D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+today = date.today()
+date = today.strftime("%m/%d/%y")
 
 def fileCheck():
     for word in letters:
         if (os.path.isfile(f'{word}:\\Autorun.inf')):
-            print(f'Autorun.inf found on {word}')
+            logadd("[#]", f'[{date}]', f'Autorun.inf found on {word}')
             return word
             break
         continue
     else:
-        print("Autorun.inf not found")
-        #logadd("?", '{date}', "could not find Autorun.inf on any drive")
+        logadd("[?]", f'[{date}]', "could not find Autorun.inf on any drive")
