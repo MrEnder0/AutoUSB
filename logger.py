cfde7bd40d8754ecea24624a68583748f1c8e89f
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-01-16 14:35:58 -0600

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,3 +1,9 @@
+import time
+
 def logadd(type, date, message):
     log = open('storage/logs.log', 'a')
     log.write(f'{type} {date} {message} \n')
+
+def logclear():
+    log = open('storage/logs.log', 'w+')
+    log = ""
