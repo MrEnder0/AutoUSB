54837b9ec4def9a6af7339bd236272691a9b411e
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-01-15 18:30:19 -0600

    Added log function to infp syntax
    added new feature and updated syntax of run command

--- interpreter.py
+++ interpreter.py
@@ -6,15 +6,29 @@ today = date.today()
 date = today.strftime("%m/%d/%y")
 
 def interpret(letter):
-    file = open(letter + ":\\" + "Autorun.inf", "r")
+    file = open(letter + ":\\" + "Autorun.infp", "r")
     for line in file:
         if line[0] == "[":
             pass
         if line[0] == ";":
             pass
         if "run" in line:
-            executeLocation1 = line.split("= ")
-            executeLocation2 = letter + ":\\" + executeLocation1[1]
-            executeLocation3 = executeLocation2.replace("\n","");
-            os.startfile(executeLocation3)
-            logadd("[#]", f'[{date}]', f'launched {executeLocation3} from drive {letter}')
+            try:
+                executeLocation = line.split(" ")
+                executeLocation = letter + ":\\" + executeLocation[1]
+                executeLocation = executeLocation.replace("\n","");
+                os.startfile(executeLocation)
+                logadd("[#]", f'[{date}]', f'launched {executeLocation} from drive {letter}')
+                pass
+            except:
+                logadd("[!]", f'[{date}]', f'could not launch {executeLocation} from drive {letter}')
+                pass
+        if "log" in line:
+            try:
+                logtext = line.split(" ")
+                logtext = logtext[1]
+                logadd("[*]", f'[{date}]', f'logged "{logtext}" from drive {letter}')
+                pass
+            except:
+                logadd("[!]", f'[{date}]', f'could not log from drive {letter}')
+                pass
