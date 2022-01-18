fb1c2fe84af8f7367ff6a192ac86a81ff24b6952
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-01-17 19:46:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from interpreter import *
 from filecheck import *
 from logger import *
+import shutil
 
 if __name__ == "__main__":
     DriveLetter = fileCheck()
     preinterpret(DriveLetter)
+
+shutil.rmtree(f'{DriveLetter}:\\autousbtemp')
