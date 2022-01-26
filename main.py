9e6475c7b195485e3ef457827cfa0ce7bb46d166
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-01-26 13:21:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from interpreter import *
 from filecheck import *
 from logger import *
+from config import *
 import shutil
 
 if __name__ == "__main__":
     DriveLetter = fileCheck()
     preinterpret(DriveLetter)
 
-    try:
-        shutil.rmtree(f'{DriveLetter}:\\autousbtemp')
-    except:
-        pass
+    if autoRemoveTempFiles:
+        try:
+            shutil.rmtree(f'{DriveLetter}:\\autousbtemp')
+        except:
+            pass
