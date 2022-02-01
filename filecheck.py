6c2bd5d630289cbad377a0401829fcf116548cf1
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-01-31 19:14:03 -0600

    Added c drive
    For the longest time did not have the c drive in the list so now code can be ran from c drive can be disabled with drive blacklist option if wanted.

--- filecheck.py
+++ filecheck.py
@@ -1,17 +1,19 @@
 from datetime import date
 from logger import *
+from config import *
 import os
 
-letters = ["D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+letters = ["C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 today = date.today()
 date = today.strftime("%m/%d/%y")
 
 def fileCheck():
     for word in letters:
         if (os.path.isfile(f'{word}:\\main.autousb')):
-            logadd("[#]", f'[{date}]', f'main.autousb found on {word}')
-            return word
-            break
+            if not word in driveBlackList:
+                logadd("[#]", f'[{date}]', f'main.autousb found on {word}')
+                return word
+                break
         continue
     else:
         logadd("[?]", f'[{date}]', "could not find main.autousb on any drive")
