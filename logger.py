e62b7db2ae6d29938a9f1b60a055f1a0d0d4c9b0
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-03-06 20:33:18 -0600

    Removed unneeded whitespace

--- logger.py
+++ logger.py
@@ -1,7 +1,6 @@
 def logadd(type, date, message):
     log = open('storage/logs.log', 'a')
     log.write(f'{type} {date} {message}\n')
-
 def logclear():
     log = open('storage/logs.log', 'w+')
     log = ""
